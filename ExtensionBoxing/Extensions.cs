72a0ac27efb65518ff207e914eeae0f838b96ce8
Jasper Kent <software@jasperkent.com>
2022-06-22 18:08:48 +0100

    Avoid code duplication - as suggested by https://www.youtube.com/channel/UCwIT6xJiKg72QdmTHHZe70g

--- ExtensionBoxing/Extensions.cs
+++ ExtensionBoxing/Extensions.cs
@@ -2,15 +2,30 @@
 {
     public static class Extensions
     {
-        public static void Ennoble<T>(this ref T nameable)
-            where T : struct, INameable
+        //public static void Ennoble<T>(this ref T nameable)
+        //    where T : struct, INameable
+        //{
+        //    nameable.Name = "Sir " + nameable.Name;
+        //}
+
+        //public static void Ennoble(this INameable nameable)
+        //{
+        //    nameable.Name = "Sir " + nameable.Name;
+        //}
+
+        private static void PrivateEnnoble<T>(ref T nameable) where T : INameable
         {
             nameable.Name = "Sir " + nameable.Name;
         }
 
         public static void Ennoble(this INameable nameable)
         {
-            nameable.Name = "Sir " + nameable.Name;
+            PrivateEnnoble(ref nameable);
+        }
+
+        public static void Ennoble<T>(this ref T nameable) where T : struct, INameable
+        {
+            PrivateEnnoble(ref nameable);
         }
     }
 }
